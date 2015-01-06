675650e8d6abcb243301b84f8b40ddc3171be9d6
Sheleng <1323736312@qq.com>
2015-01-06 15:54:43 +0800

    修改首页布局

--- public/js/main.js
+++ public/js/main.js
@@ -55,3 +55,26 @@ var scrolltotop={
 	}
 };
 scrolltotop.init();
+
+var dataStr = '{ {% for tag in site.tags %}{% if tag[0] != site.tags.first[0] %},{% endif %}"{{ tag[0] }}":[{% for post in tag[1] %}{% if post != tag[1].first %},{% endif %}{"url":"{{post.url}}", "title":"{{post.title}}", "date":"{{post.date | date:"%d/%m/%Y"}}"}{% endfor %}]{% endfor %} }',
+    data = JSON.parse(dataStr),
+    curTag = $.query.get("tag"),
+    archieves = data[curTag];
+
+var dataStr = '
+{ 
+	{% for tag in site.tags %}
+		{% if tag[0] != site.tags.first[0] %}
+			,
+		{% endif %}
+		"{{ tag[0] }}":[
+			{% for post in tag[1] %}
+				{% if post != tag[1].first %}
+					,
+				{% endif %}
+				{"url":"{{post.url}}", "title":"{{post.title}}", "date":"{{post.date | date:"%d/%m/%Y"}}"}
+			{% endfor %}]
+	{% endfor %} }',
+    data = JSON.parse(dataStr),
+    curTag = $.query.get("tag"),
+    archieves = data[curTag];
