bc576572d4b91aa63ca8909b5947d0fb9ee527d9
Sheleng <1323736312@qq.com>
2015-01-07 10:45:05 +0800

    修改js

--- public/js/main.js
+++ public/js/main.js
@@ -16,35 +16,6 @@ jq.fn.tagcloud.defaults = {
     jq('#tag-cloud a').tagcloud();
 });
 
-var tagsInfo = '[{ {% for tag in site.tags %}{% if tag[0] != site.tags.first[0] %},{% endif %}"{{ tag[0] }}":[{% for post in tag[1] %}{% if post != tag[1].first %},{% endif %}{"url":"{{post.url}}", "title":"{{post.title}}", "date":"{{post.date | date:"%d/%m/%Y"}}"}{% endfor %}]{% endfor %} }]',
-    tagsJson = JSON.parse(tagsInfo);
-
-function showTag(tagStr) {
-    jq.getJSON(tagsJson,
-    function(data) {
-      jq('#show-tag').empty(content);
-      var content = "<h2>分类：" + tagStr + "</h2><ul class=\"posts\">";
-      var count = 0;
-      jq.each(data,
-      function(i, item) {
-        jq.each(item.tags,
-        function(j, tag) {
-          if (tag == tagStr) {
-            content += "<li class=\"listing-item\"><time datetime=\"" + item.date + "\">" + item.date + "</time><a href=\"" + item.url + "\">" + item.title + "</a></li>";
-            count++;
-          }
-
-        });
-      });
-      if (count > 0) {
-        content += "</ul>";
-        postNumStr = "<span>（" + count + "篇文章）</span>";
-        jq('#show-tag').append(content);
-        jq('#show-tag>h2').append(postNumStr);
-      }
-    });
-}
-
 var scrolltotop={
 	setting:{
 		startline:100, //起始行
