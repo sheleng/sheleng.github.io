562d12a59fc9340cea78d9aeec3fe8382cb7436d
Sheleng <1323736312@qq.com>
2015-01-09 16:14:46 +0800

    添加标签页

--- public/js/main.js
+++ public/js/main.js
@@ -77,7 +77,7 @@ scrolltotop.init();
 function getQueryParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]); return null;
+    if (r != null) return r[2]; return null;
 }
 
 function showTagPost(){
