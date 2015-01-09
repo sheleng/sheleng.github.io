52b91860902a137d6fcc9f08e20c12f31a905e0b
Sheleng <1323736312@qq.com>
2015-01-09 15:02:30 +0800

    添加标签页

--- public/js/main.js
+++ public/js/main.js
@@ -73,3 +73,9 @@ var scrolltotop={
 	}
 };
 scrolltotop.init();
+
+function getQueryParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
